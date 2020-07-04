4de71f97b43581fd76a5661495a9f2b131c0186f
Ivan <4esoquevedoi@gmail.com>
2020-07-04 11:11:51 +0200

    navbar tests

--- src/Components/common/Navbar/Navbar.test.js
+++ src/Components/common/Navbar/Navbar.test.js
@@ -1,47 +1,28 @@
 import React from "react";
 import { screen } from "@testing-library/dom";
 import "@testing-library/jest-dom/extend-expect";
-import matchMediaPolyfill from "mq-polyfill";
+import { Context as ResponsiveContext } from "react-responsive";
 
 import { render } from "Utils/testUtils";
 import images from "Assets/Navbar";
 
 import Navbar from "./index";
 
-beforeAll(() => {
-  matchMediaPolyfill(window);
-  window.resizeTo = function resizeTo(width, height) {
-    Object.assign(this, {
-      innerWidth: width,
-      innerHeight: height,
-      outerWidth: width,
-      outerHeight: height,
-    }).dispatchEvent(new this.Event("resize"));
-  };
-});
-
-test("Navbar image changes on resize viewport", async () => {
+test("Navbar image renders correctly", async () => {
   render(<Navbar />);
-
-  window.resizeTo(1200, 1200);
   const bigWindowImage = screen.getByAltText(
     "Volkswagen Financial Services - The Key To Mobility"
   ).src;
   expect(bigWindowImage).toBe(images.desktop);
-
-  //   window.resizeTo(550, 550);
-
-  //   const smallWindowImage = screen.getByAltText(
-  //     "Volkswagen Financial Services - The Key To Mobility"
-  //   ).src;
-
-  //   expect(smallWindowImage).toBe(images.mobile);
 });
 
-test("Navbar image changes on resize viewport", async () => {
-  render(<Navbar />);
-
-  window.resizeTo(550, 550);
+test("Navbar image changes on mobile devices", async () => {
+  const mobileSize = 599;
+  render(
+    <ResponsiveContext.Provider value={{ width: mobileSize }}>
+      <Navbar />
+    </ResponsiveContext.Provider>
+  );
 
   const smallWindowImage = screen.getByAltText(
     "Volkswagen Financial Services - The Key To Mobility"
